fd4f6938d892e0135fce596fdf71f222bda4f039
takesa <takesa@GMKTEC>
2025-09-13 20:55:51 +0900

    プロジェクト ファイルを追加します。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SkyWatcherMotorControllerMonitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.serialPortIn = new System.IO.Ports.SerialPort(this.components);
+            this.serialPortOut = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxPortIn = new System.Windows.Forms.ComboBox();
+            this.comboBoxPortOut = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonLogClear = new System.Windows.Forms.Button();
+            this.textBoxLogMean = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBoxLog.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxLog.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.textBoxLog.Location = new System.Drawing.Point(12, 42);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(311, 399);
+            this.textBoxLog.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Log";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(358, 14);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 2;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // serialPortIn
+            // 
+            this.serialPortIn.BaudRate = 115200;
+            this.serialPortIn.PortName = "COM6";
+            this.serialPortIn.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortIn_DataReceived);
+            // 
+            // serialPortOut
+            // 
+            this.serialPortOut.BaudRate = 115200;
+            this.serialPortOut.PortName = "COM4";
+            this.serialPortOut.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPortOut_DataReceived);
+            // 
+            // comboBoxPortIn
+            // 
+            this.comboBoxPortIn.FormattingEnabled = true;
+            this.comboBoxPortIn.Location = new System.Drawing.Point(184, 16);
+            this.comboBoxPortIn.Name = "comboBoxPortIn";
+            this.comboBoxPortIn.Size = new System.Drawing.Size(70, 20);
+            this.comboBoxPortIn.TabIndex = 3;
+            // 
+            // comboBoxPortOut
+            // 
+            this.comboBoxPortOut.FormattingEnabled = true;
+            this.comboBoxPortOut.Location = new System.Drawing.Point(281, 16);
+            this.comboBoxPortOut.Name = "comboBoxPortOut";
+            this.comboBoxPortOut.Size = new System.Drawing.Size(70, 20);
+            this.comboBoxPortOut.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "→";
+            // 
+            // buttonLogClear
+            // 
+            this.buttonLogClear.Location = new System.Drawing.Point(41, 16);
+            this.buttonLogClear.Name = "buttonLogClear";
+            this.buttonLogClear.Size = new System.Drawing.Size(47, 20);
+            this.buttonLogClear.TabIndex = 6;
+            this.buttonLogClear.Text = "Clear";
+            this.buttonLogClear.UseVisualStyleBackColor = true;
+            this.buttonLogClear.Click += new System.EventHandler(this.buttonLogClear_Click);
+            // 
+            // textBoxLogMean
+            // 
+            this.textBoxLogMean.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLogMean.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBoxLogMean.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.textBoxLogMean.Location = new System.Drawing.Point(329, 42);
+            this.textBoxLogMean.Multiline = true;
+            this.textBoxLogMean.Name = "textBoxLogMean";
+            this.textBoxLogMean.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLogMean.Size = new System.Drawing.Size(428, 399);
+            this.textBoxLogMean.TabIndex = 7;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 454);
+            this.Controls.Add(this.textBoxLogMean);
+            this.Controls.Add(this.buttonLogClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxPortOut);
+            this.Controls.Add(this.comboBoxPortIn);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxLog);
+            this.Name = "MainForm";
+            this.Text = "Sky-Watcher Motor Controller Monitor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.IO.Ports.SerialPort serialPortIn;
+        private System.IO.Ports.SerialPort serialPortOut;
+        private System.Windows.Forms.ComboBox comboBoxPortIn;
+        private System.Windows.Forms.ComboBox comboBoxPortOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonLogClear;
+        private System.Windows.Forms.TextBox textBoxLogMean;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace SkyWatcherMotorControllerMonitor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string lastCommand = "";
+        private int lastChannel = 0;
+        private string lastParam = "";
+
+        private void LoadAvailablePorts()
+        {
+            comboBoxPortIn.Items.AddRange(SerialPort.GetPortNames());
+            if (comboBoxPortIn.Items.Count > 0)
+            {
+                for (int i=0; i < comboBoxPortIn.Items.Count; i++)
+                {
+                    if (comboBoxPortIn.Items[i].ToString() == serialPortIn.PortName)
+                    {
+                        comboBoxPortIn.SelectedIndex = i;
+                        break;
+                    }
+                }
+            }
+
+            comboBoxPortOut.Items.AddRange(SerialPort.GetPortNames());
+            if (comboBoxPortOut.Items.Count > 0)
+            {
+                for (int i = 0; i < comboBoxPortOut.Items.Count; i++)
+                {
+                    if (comboBoxPortOut.Items[i].ToString() == serialPortOut.PortName)
+                    {
+                        comboBoxPortOut.SelectedIndex = i;
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPortIn.IsOpen)
+                {
+                    serialPortIn.Close();
+                    serialPortOut.Close();
+                    buttonConnect.ForeColor = Color.Black;
+                    //Debug.WriteLine("Port Close");
+                    ShowReceivedData("Port Close" + Environment.NewLine);
+
+                }
+                else
+                {
+                    serialPortIn.Open();
+                    serialPortOut.Open();
+                    buttonConnect.ForeColor = Color.Red;
+                    //Debug.WriteLine("Port Open");
+                    //Debug.WriteLine(serialPortIn.PortName);
+                    //Debug.WriteLine(serialPortOut.PortName);
+                    //textBoxLog.Clear();
+                    ShowReceivedData("Port Open: "
+                        + serialPortIn.PortName 
+                        + " " 
+                        + serialPortOut.PortName 
+                        + Environment.NewLine);
+                    ShowReceivedData("Wait for SynScanPro to Connect." +Environment.NewLine);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        /// <summary>
+        /// SynScanProからのデータを受信したときのイベントハンドラ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void serialPortIn_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                //string data = serialPortIn.ReadLine();
+                string data = serialPortIn.ReadExisting();
+                //Debug.WriteLine("Send: " + data);
+                ShowReceivedData("--> " + data + Environment.NewLine);
+
+                if (data.Substring(0, 1) == ":")
+                {
+                    lastCommand = data.Substring(1, 1);
+                    lastChannel = int.Parse(data.Substring(2, 1));
+                    lastParam = "";
+                    string mean = "";
+                    switch (lastCommand)
+                    {
+                        case "b":
+                            mean = "Inquire Timer Interrupt Freq";
+                            break;
+                        case "e":
+                            mean = "Inquire Motor Board Version";
+                            break;
+                        case "f":
+                            mean = "Inquire Status";
+                            break;
+                        case "q":
+                            mean = "Extended Inquire";
+                            break;
+                        case "s":
+                            mean = "Inquire PEC period";
+                            break;
+                        case "P":
+                            mean = "Set AutoGuide Speed";
+                            lastParam = data.Substring(3);
+                            break;
+                        case "X":
+                            mean = "X_" +data.Substring(3, 2);
+                            lastCommand = data.Substring(3, 2);
+                            if (data.Length > 5)
+                                lastParam = data.Substring(5);
+                            break;
+                        case "V":
+                            mean = "Set Polar Scope LED brightness";
+                            lastParam = data.Substring(3);
+                            break;
+                        default:
+                            mean = "unknown";
+                            break;
+                    }
+                    mean += " Ch" + lastChannel.ToString();
+                    if (!lastParam.Equals(""))
+                        mean += " Param:" + InsertSpaceEvery8Chars(lastParam);
+                    ShowReceivedMean("--> " + mean + Environment.NewLine);
+                    
+                }
+                serialPortOut.Write(data);  //転送
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return;
+            }
+
+
+        }
+
+        /// <summary>
+        /// モーターコントローラーからのデータを受信したときのイベントハンドラ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void serialPortOut_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = serialPortOut.ReadExisting();
+                //Debug.WriteLine("Receive: " + data);
+                ShowReceivedData("<-- " + data + Environment.NewLine);
+
+                if (data.Substring(0, 1) == "=")
+                {
+                    string mean = "";
+                    if (data.Length > 1)
+                        mean = InsertSpaceEvery8Chars(data.Substring(1));
+                    ShowReceivedMean("<--  " + mean + Environment.NewLine);
+
+                    //if (data.Substring(1).Length >= 32)
+                    //{
+                    //    long long1 = DecodeBigEndianHexToLong(data.Substring(1, 8));
+                    //    long long2 = DecodeBigEndianHexToLong(data.Substring(9, 8));
+                    //    long long3 = DecodeBigEndianHexToLong(data.Substring(17, 8));
+                    //    long long4 = DecodeBigEndianHexToLong(data.Substring(25, 8));
+                    //    mean = long1.ToString() + " " + long2.ToString() + " " + long3.ToString() + " " + long4.ToString();
+                    //    ShowReceivedMean("     " + mean + Environment.NewLine);
+                    //}
+
+                } else if (data.Substring(0, 1) == "!")
+                {
+                    //エラーメッセージ
+                    ShowReceivedMean("<--  Error" + Environment.NewLine);
+                }
+
+                serialPortIn.Write(data);  //転送
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadAvailablePorts();
+
+        }
+
+        private void ShowReceivedData(string text)
+        {
+            if (textBoxLog.InvokeRequired)
+            {
+                textBoxLog.Invoke(new Action<string>(ShowReceivedData), text);
+            }
+            else
+            {
+                //textBoxLog.AppendText(text + "\r\n");
+                textBoxLog.AppendText(text);
+            }
+        }
+
+        private void ShowReceivedMean(string text)
+        {
+            if (textBoxLogMean.InvokeRequired)
+            {
+                textBoxLogMean.Invoke(new Action<string>(ShowReceivedMean), text);
+            }
+            else
+            {
+                textBoxLogMean.AppendText(text);
+            }
+        }
+
+
+        private void buttonLogClear_Click(object sender, EventArgs e)
+        {
+            textBoxLog.Clear();
+            textBoxLogMean.Clear();
+        }
+
+        private string InsertSpaceEvery8Chars(string text)
+        {
+            int chunkSize = 8;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            for (int i = 0; i < text.Length; i += chunkSize)
+            {
+                int length = Math.Min(chunkSize, text.Length - i);
+                sb.Append(text.Substring(i, length));
+
+                // 最後のチャンクでなければスペースを追加
+                if (i + chunkSize < text.Length)
+                    sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+
+        private long DecodeSkyWatcherHexToLong(string hex)
+        {
+            // 入力チェック：6文字または8文字の16進文字列
+            if (hex.Length != 6 && hex.Length != 8)
+                throw new ArgumentException("6桁または8桁の16進文字列が必要です");
+
+            // 2文字ずつ分割してバイト順を逆にする（リトルエンディアン）
+            var bytes = new List<string>();
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                bytes.Add(hex.Substring(i, 2));
+            }
+            bytes.Reverse(); // バイト順を逆転
+
+            string reordered = string.Join("", bytes);
+
+            // 16進数として long に変換
+            return Convert.ToInt64(reordered, 16);
+        }
+
+        private long DecodeBigEndianHexToLong(string hex)
+        {
+            // 入力チェック：6文字または8文字の16進文字列
+            if (hex.Length != 6 && hex.Length != 8)
+                throw new ArgumentException("6桁または8桁の16進文字列が必要です");
+
+            // そのまま順番を保持して変換（ビッグエンディアン）
+            return Convert.ToInt64(hex, 16);
+        }
+
+    }
+}
